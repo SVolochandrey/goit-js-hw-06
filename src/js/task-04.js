6231953b91cf507838cfc8a96771a646ce7fd015
SVolochandrey <volochan.andrey@gmail.com>
2023-01-30 20:31:36 +0200

    done 5th task

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -14,13 +14,13 @@ const valueEl = document.querySelector("#value");
 const buttonDecrementEl = document.querySelector('[data-action="decrement"]');
 const buttonIncrementEl = document.querySelector('[data-action="increment"]');
 
-const onButtonDecrementClick = (event) => {
+const onButtonDecrementClick = () => {
 console.log('You clicked -1');
 counterValue.decrement();
 valueEl.textContent = counterValue.value;
 };
 
-const onButtonIncrementClick = (event) => {
+const onButtonIncrementClick = () => {
 console.log('You clicked +1');
 counterValue.increment();
 valueEl.textContent = counterValue.value;
---
+++ src/js/task-05.js
@@ -0,0 +1,10 @@
+const inputEl = document.querySelector('#name-input');
+const nameUserEl = document.querySelector('#name-output');
+
+const onInputChange = (event) => {
+inputEl.value === ""
+? nameUserEl.textContent = "Anonymous"
+:nameUserEl.textContent = inputEl.value
+};
+
+inputEl.addEventListener("input", onInputChange);
