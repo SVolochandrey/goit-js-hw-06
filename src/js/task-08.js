c429241b935d66decb77e9eeb1d2c59cff9e936c
SVolochandrey <volochan.andrey@gmail.com>
2023-02-03 08:21:09 +0200

    done 8th task

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector(".login-form");
+const formData = {
+email: "",
+password: "",
+};
 
+formEl.addEventListener("submit", handleFormSubmit);
+
+function handleFormSubmit(event){
+event.preventDefault();
+
+
+const elements = event.currentTarget.elements;
+
+if(elements.email.value === "" || elements.password.value === "") {
+    alert('Заповніть всі поля!')
+}
+else {formData.email = elements.email.value;
+formData.password = elements.password.value;;
+
+console.log(formData)};
+}
